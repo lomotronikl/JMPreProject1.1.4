d732f1bcd0ea0b71a029d1a80721d38b720e7814
lomotronikl@gmail.com <bpwAxe08>
2021-08-19 19:12:19 +0800

    move util classes -> util

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,6 +1,7 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.ConnectionUtils;
 
 import java.sql.*;
 import java.util.LinkedList;
--- src/main/java/jm/task/core/jdbc/util/MySQLConnectionUtils.java
+++ src/main/java/jm/task/core/jdbc/util/MySQLConnectionUtils.java
@@ -1,4 +1,4 @@
-package jm.task.core.jdbc.dao;
+package jm.task.core.jdbc.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
